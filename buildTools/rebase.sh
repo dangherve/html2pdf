4f63966ad7c8bebd2212ab593b9b51467b850550
dangherve <dangherve@gmail.com>
2026-03-12 13:47:43 -0400

    autorebase tools

---
+++ buildTools/rebase.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+set -e
+git remote add upstream https://github.com/spipu/html2pdf.git
+git fetch upstream
+git rebase upstream/master
+git remote set-url origin git@github.com:dangherve/html2pdf.git
+git push --force --set-upstream origin master
